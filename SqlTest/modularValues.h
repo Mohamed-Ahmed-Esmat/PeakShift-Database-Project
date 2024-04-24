eb160fbae36ff677ebfd7ec3e9d860740a471f10
Trimbex <111075379+Trimbex@users.noreply.github.com>
2024-04-24 23:34:53 +0200

    backend for adduser complete

---
+++ SqlTest/modularValues.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <string>
+// MODULAR VALUES FOR SQL CONNECTION
+
+
+using namespace std;
+class Modules
+{
+public:
+	string serverName = "DESKTOP-O086REE";
+	string dataBaseName = "PeakShift";
+};
